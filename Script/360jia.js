67c9cc3ddab07cb74e98c6c02011a6fffe43fb5a
mmxxooyy <25954771+mmxxooyy@users.noreply.github.com>
2024-07-10 01:05:29 +0800

    Update 360jia.js

--- Script/360jia.js
+++ Script/360jia.js
@@ -1,12 +1,12 @@
-var json = JSON.parse($response.body);
+var data = JSON.parse($response.body);
 
 // 删除 - 我的页面推广语
-delete json.activity;
+delete data.activity;
 
 // 删除 - 底栏
-delete json.tab_conf;
+data.tab_conf = {};
 
 // 删除 - 广告配置
-delete json.ad_conf;
+delete data.ad_conf;
 
 $done({ body: JSON.stringify(json) });
