49fd25f75fbccdd2e7efb4dc0fb37a659082dece
mmxxooyy <25954771+mmxxooyy@users.noreply.github.com>
2024-07-10 08:55:17 +0800

    Update 360jia.js

--- Script/360jia.js
+++ Script/360jia.js
@@ -1,14 +1,23 @@
-var data = JSON.parse($response.body);
-
-// 删除 - 我的页面推广语
-delete data.activity;
-
-// 删除 - 底栏
-data.tab_conf = {};
+let data = JSON.parse($response.body);
 
 // 删除 - 广告配置
 delete data.ad_conf;
 
+// 删除 - 底栏
+// data.tab_conf = {};
+
+// 删除 - 我的页面推广语
+delete data.activity;
+
+// 删除 - 我的-服务
 data.settingServiceConfig.data = [];
 
-$done({ body: JSON.stringify(data) });
+delete data.tab_conf["tab_360Mall"];
+data.tab_conf["tab_CloudMall"]["on"] = "1";
+data.get_new_device_activity = {};
+data.get_new_device_activity_copy = {};
+data.video_cloud_banner = {};
+data.video_cloud_card = {};
+data.get_new_device_activity_copy = {};
+
+$done({body: JSON.stringify(data)});
